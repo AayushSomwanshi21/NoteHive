35c1f73c450e3cc75c46d2449891f99acc596587
AayushSomwanshi21 <aayushsomwanshi21@gmail.com>
2024-07-10 19:42:47 +0530

    connected DB

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoDB = "mongodb+srv://aayushsomwanshi21:altair2104@cluster0.gyt1hyu.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+async function connectDB() {
+    try {
+        await mongoose.connect(mongoDB);
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error("MongoDB connection error: ", err);
+    }
+}
+
+module.exports = connectDB;
